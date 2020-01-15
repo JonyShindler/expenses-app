824e1b3021aed65acd50ccff41e063a1322dd798
Jonathan Shindler <jonathanshindler@hotmail.com>
2020-01-15 20:17:19 +0000

    Added expense details page.

--- web/src/main/java/berkeley/creations/expenses/controller/ExpenseController.java
+++ web/src/main/java/berkeley/creations/expenses/controller/ExpenseController.java
@@ -5,11 +5,14 @@ import berkeley.creations.expenses.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.StringUtils;
 
+import javax.validation.Valid;
 import java.util.Set;
 
 @RequestMapping("/expenses")
@@ -28,7 +31,41 @@ public class ExpenseController {
     public String showAllExpenses(Model model) {
         Set<Expense> expenses = expenseService.findAll();
         model.addAttribute("expenses", expenses);
-        return "expense/allExpenses";
+        return "expenses/allExpenses";
+    }
+
+    @GetMapping("/{expenseId}")
+    public ModelAndView showExpense(@PathVariable("expenseId") Long expenseId) {
+        ModelAndView mav = new ModelAndView("/expenses/expenseDetails");
+        mav.addObject(expenseService.findById(expenseId));
+        return mav;
+    }
+
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model) {
+        Expense expense = new Expense();
+        model.addAttribute("expense", expense);
+        return "expenses/createOrUpdateExpenseForm";
+    }
+
+    @InitBinder("expense")
+    public void initOwnerBinder(WebDataBinder binder) {
+        binder.setDisallowedFields("id");
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Expense expense, BindingResult result, ModelMap model) {
+        //TODO check it is not duplicate
+        //result.rejectValue("name", "duplicate", "already exists");
+
+        if (result.hasErrors()) {
+            model.put("expense", expense);
+            return "pets/createOrUpdatePetForm";
+        } else {
+            expenseService.save(expense);
+            return "redirect:/expenses/" + expense.getId();
+        }
     }
 
 }
